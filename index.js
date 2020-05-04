f982dc9ddb822f67a0b254e961e431eb11e65dc0
myxomopx <myxomopx@gmail.com>
2020-05-04 17:52:22 +0500

    Добавил подключение к БД

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
+const dbClient = require("./src/dao/DBClient.js")
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+// static files
+app.use('/', express.static('./src/web'))
+
+
+app.get('/test', (req, res) => res.send('Hello World!'))
+
+app.post('/measurements', (req, res) => {
+	const data = res.body;
+	// insert data into SQL (dbClient)
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
+
+dbClient.query('SELECT * FROM measurement', (err, res) => {
+  console.log(res.fields)
+  console.log(res.rows)
+  dbClient.end()
+})
---
+++ src/dao/DBClient.js
@@ -0,0 +1,12 @@
+const { Pool, Client } = require('pg')
+
+const client = new Client({
+  user: 'a50ghz_user',
+  host: 'myxomopx.ru',
+  database: 'a50ghz',
+  password: 'magic_measurements',
+  port: 10433,
+})
+client.connect();
+
+module.exports = client
